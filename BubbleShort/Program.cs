10db1452cdf6c461483f1c5fc77b4f108789a2ad
SalmanLS <salmanluthfi456@gmail.com>
2022-10-03 21:08:03 +0700

    create namespace dan kelas program

--- BubbleShort/Program.cs
+++ BubbleShort/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+
+    }
+}
